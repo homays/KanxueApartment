2132bec4b6329d601228beaadd4042edaa9a7863
Arrebol <2362552845@qq.com>
2024-07-15 20:37:42 +0800

    feat: 支付方式接口开发

--- web/web-admin/src/main/java/com/arrebol/web/admin/controller/apartment/PaymentTypeController.java
+++ web/web-admin/src/main/java/com/arrebol/web/admin/controller/apartment/PaymentTypeController.java
@@ -2,8 +2,10 @@ package com.arrebol.web.admin.controller.apartment;
 
 import com.arrebol.common.result.Result;
 import com.arrebol.model.entity.PaymentType;
+import com.arrebol.web.admin.service.PaymentTypeService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.annotation.Resource;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -13,21 +15,26 @@ import java.util.List;
 @RestController
 public class PaymentTypeController {
 
+    @Resource
+    private PaymentTypeService paymentTypeService;
+
     @Operation(summary = "查询全部支付方式列表")
     @GetMapping("list")
     public Result<List<PaymentType>> listPaymentType() {
-        return Result.ok();
+        return Result.ok(paymentTypeService.list());
     }
 
     @Operation(summary = "保存或更新支付方式")
     @PostMapping("saveOrUpdate")
     public Result saveOrUpdatePaymentType(@RequestBody PaymentType paymentType) {
+        paymentTypeService.saveOrUpdate(paymentType);
         return Result.ok();
     }
 
     @Operation(summary = "根据ID删除支付方式")
     @DeleteMapping("deleteById")
     public Result deletePaymentById(@RequestParam Long id) {
+        paymentTypeService.removeById(id);
         return Result.ok();
     }
 
